f6e96aafea533f191f48a59931b0ca94f7ad3f50
gongtao02 <gongtao02@meituan.com>
2023-03-22 23:15:28 +0800

    feat: save

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -2,8 +2,117 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::fmt; // 导入 `fmt`
+
+// 带有两个数字的结构体。推导出 `Debug`，以便与 `Display` 的输出进行比较。
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+// 实现 `MinMax` 的 `Display`。
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // 使用 `self.number` 来表示各个数据。
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+// 为了比较，定义一个含有具名字段的结构体。
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// 类似地对 `Point2D` 实现 `Display`
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // 自定义格式，使得仅显示 `x` 和 `y` 的值。
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+// 推导 `Structure` 的 `fmt::Debug` 实现。
+// `Structure` 是一个包含单个 `i32` 的结构体。
+#[derive(Debug)]
+struct Structure(i32);
+
+// 将 `Structure` 放到结构体 `Deep` 中。然后使 `Deep` 也能够打印。
+#[derive(Debug)]
+struct Deep(Structure);
+
+// impl fmt::Display for Vec<T> {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "{}", self[0])
+//     }
+// }
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "red:{}, green:{}, blue:{}",
+            &self.red, &self.green, &self.blue
+        )
+    }
+}
 
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!", "world");
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+    // `Structure` 也可以打印！
+    println!("Now {:?} will print!", Structure(3));
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    let c = Color {
+        red: 128,
+        green: 255,
+        blue: 90,
+    };
+    println!("{}", c);
 }
